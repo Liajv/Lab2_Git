b923df3b529c70aa56b586573dbd1bbcb26b3973
Jonathan Victor <jonathanvictor000@gmail.com>
2024-09-06 12:55:38 -0400

    added a username variable

--- Victor.py
+++ Victor.py
@@ -3,4 +3,5 @@ Jonathan Victor
 lab 2, git and github in VS code
 Sep 6
 """
-print("Welcome to Git commands ")
+username = input("Enter a username: ")
+print(f"Welcome to Git commands {username} ")
